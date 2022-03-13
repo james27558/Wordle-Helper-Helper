82c00fbc1d9d0b7a941ffb160818a0d8eeff7b37
james27558 <dev11mee1@hotmail.co.uk>
2022-03-13 12:52:44 +0000

    Move code into class with getters/setters

--- main.py
+++ main.py
@@ -1,21 +1,57 @@
+from enum import Enum
+
 import requests
 from bs4 import BeautifulSoup
 
 
+class WordleHelperHelper:
+    def __init__(self):
+        self.guesses = []
+        self.words = []
+        self.bad_letters = []
+        self.good_letters = []
+        self.placed_letters = [[] * 5]
 
-headers = requests.utils.default_headers()
-headers.update({
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
-})
+        self.search_mode = SearchMode.LINEAR
 
-URL = "https://gist.githubusercontent.com/cfreshman/a7b776506c73284511034e63af1017ee/raw/845966807347a7b857d53294525263408be967ce/wordle-nyt-answers-alphabetical.txt"
+        self.fetchWords()
 
-page = requests.get(URL, headers=headers)
+    def fetchWords(self):
+        headers = requests.utils.default_headers()
+        headers.update({
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+        })
+
+        URL = "https://gist.githubusercontent.com/cfreshman/a7b776506c73284511034e63af1017ee/raw/845966807347a7b857d53294525263408be967ce/wordle-nyt-answers-alphabetical.txt"
+        page = requests.get(URL, headers=headers)
+
+        soup = BeautifulSoup(page.content, "lxml")
+
+        self.words = soup.p.text.split("\n")
+
+    def setGuess(self, guess):
+        self.guesses.append(guess)
+        for c in guess:
+            self.good_letters.append(c)
+
+    def setBadLetter(self, bad_letter):
+        self.bad_letters.append(bad_letter)
+
+    def setGoodLetter(self, good_letter):
+        self.good_letters.append(good_letter)
+
+    def softReset(self):
+        """
+        Resets the fields such that the guesses are kept but the good, bad and placed letters are not
+        :return:
+        """
+
+        self.good_letters = []
+        self.bad_letters = []
+        self.placed_letters = [[] * 5]
 
 
-soup = BeautifulSoup(page.content, "lxml")
-res = soup.p.text
-words = res.split("\n")
-guesses = []
+class SearchMode(Enum):
+    LINEAR = 0
 
-print(words)
+whh = WordleHelperHelper()
